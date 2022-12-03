76991a18980753e95b2a4752b662ac505e81bf19
Chloe Tan <thryec@gmail.com>
2022-12-03 11:49:41 +0800

    skeleton

---
+++ README.md
@@ -0,0 +1,5 @@
+# Collateralized Vault
+
+https://github.com/yieldprotocol/mentorship2022/issues/5
+
+## Objectives
---
+++ test/Vault.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Vault.sol";
+
+contract VaultTest is Test {
+    Vault public vault;
+
+    function setUp() public {
+        counter = new Counter();
+        counter.setNumber(0);
+    }
+}
